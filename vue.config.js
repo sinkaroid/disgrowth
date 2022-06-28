f47b37eb51987f674e99d01471d77d0fe64ebaa9
sinkaroid <bibsindra@gmail.com>
2022-06-28 10:33:08 +0700

    build: remove transpileDependencies options

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+// addition starts here
+const TerserPlugin = require('terser-webpack-plugin')
+const isProd = process.env.NODE_ENV === 'production'
+// addition ends here
+
+module.exports = {
+    // addition ends here
+    // and keep what was already here
+    configureWebpack: {
+        optimization: {
+            minimize: true,
+            minimizer: isProd ? [
+                new TerserPlugin({
+                    terserOptions: {
+                        ecma: 6,
+                        compress: { drop_console: true },
+                        output: { comments: false, beautify: false }
+                    }
+                })
+            ] : []
+        }
+    }
+}
