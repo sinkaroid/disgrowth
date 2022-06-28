34d7ecfdef55b2c91cfb26e061f53488583feb0d
sinkaroid <bibsindra@gmail.com>
2022-06-28 10:34:55 +0700

    feat: disgrowth-visualization

---
+++ src/components/LineChart.vue
@@ -0,0 +1,120 @@
+<script>
+import { Line, mixins } from "vue-chartjs";
+import { getRandomColor } from "../../src/base"
+ import { getInfo } from '../base';
+
+export default {
+  extends: Line,
+  mixins: [mixins.reactiveData],
+  data() {
+    return {
+      chartData: "",
+        options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: true,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+  async created() {
+    await getInfo()
+      .then((response) => {
+        // JSON responses are automatically parsed.
+        
+        //map all date inside response.stats_daily, and filter empty arrays.
+        let dates = response.stats_daily.map((item) => {
+          return item.date;
+        });
+
+        let filteredDates = dates.filter((item) => {
+          return item !== "";
+        });
+
+        //membalikan array dari kanan ke kiri.
+        let reversedDates = filteredDates.reverse();
+
+        //map all total_server inside response.stats_daily, and filter empty or "0" arrays.
+        let total_server = response.stats_daily.map((item) => {
+          return item.total_server;
+        });
+
+        let filteredTotal_server = total_server.filter((item) => {
+          return item !== "0" && item !== "";
+        });
+
+        //membalikan array dari kanan ke kiri.
+        let reversedTotal_server = filteredTotal_server.reverse();
+        
+
+        this.title = response.data.title;
+        this.chartData = {
+          labels: reversedDates,
+          datasets: [
+            {
+              label: `${response.data.title}'s total guilds`,
+              backgroundColor: getRandomColor(),
+              pointBackgroundColor: "white",
+              borderWidth: 1,
+              pointBorderColor: "#249EBF",
+              data: reversedTotal_server,
+            },
+          ],
+        };
+
+        this.chartOptions = {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+                gridLines: {
+                  display: true,
+                },
+              },
+            ],
+            xAxes: [
+              {
+                gridLines: {
+                  display: false,
+                },
+              },
+            ],
+          },
+          legend: {
+            display: true,
+          },
+          responsive: true,
+          maintainAspectRatio: false,
+        };
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
+};
+</script>
---
+++ src/components/LineChartHourly.vue
@@ -0,0 +1,120 @@
+<script>
+import { Bar, mixins } from "vue-chartjs";
+import { getRandomColor } from "../../src/base"
+ import { getInfo } from '../base';
+
+export default {
+  extends: Bar,
+  mixins: [mixins.reactiveData],
+  data() {
+    return {
+      chartData: "",
+        options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: true,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+  async created() {
+    await getInfo()
+      .then((response) => {
+        // JSON responses are automatically parsed.
+        
+        //map all date inside response.data.stats_hourly, and filter empty arrays.
+        let dates = response.stats_hourly.map((item) => {
+          return item.date;
+        });
+
+        let filteredDates = dates.filter((item) => {
+          return item !== "";
+        });
+
+        //membalikan array dari kanan ke kiri.
+        let reversedDates = filteredDates.reverse();
+
+        //map all server_growth inside response.data.stats_hourly, and filter empty or "0" arrays.
+        let server_growth = response.stats_hourly.map((item) => {
+          return item.server_growth;
+        });
+
+        let filteredserver_growth = server_growth.filter((item) => {
+          return item !== "0" && item !== "";
+        });
+
+        //membalikan array dari kanan ke kiri.
+        let reversedserver_growth = filteredserver_growth.reverse();
+        
+
+        this.title = response.data.title;
+        this.chartData = {
+          labels: reversedDates,
+          datasets: [
+            {
+              label: `${response.data.title}'s hourly guilds`,
+              backgroundColor: getRandomColor(),
+              pointBackgroundColor: getRandomColor(),
+              borderWidth: 1,
+              pointBorderColor: getRandomColor(),
+              data: reversedserver_growth,
+            },
+          ],
+        };
+
+        this.chartOptions = {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+                gridLines: {
+                  display: true,
+                },
+              },
+            ],
+            xAxes: [
+              {
+                gridLines: {
+                  display: false,
+                },
+              },
+            ],
+          },
+          legend: {
+            display: true,
+          },
+          responsive: true,
+          maintainAspectRatio: false,
+        };
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
+};
+</script>
