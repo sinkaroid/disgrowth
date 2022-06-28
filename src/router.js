ab7e215b58d209944d0fd8967c6767221a5c839d
sinkaroid <bibsindra@gmail.com>
2022-06-28 10:37:19 +0700

    fix: use redirect property instead hello world

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/Home'
+import VueChartJS from '@/views/VueChartJS'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      redirect: '/graph',
+      component: Home
+    },
+    {
+      path: '/graph',
+      name: 'VueChartJS',
+      component: VueChartJS
+    }
+  ]
+})
