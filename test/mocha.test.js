0fdcab22d2c5c36c00ca5172199b4c22caef4df2
sinkaroid <bibsindra@gmail.com>
2022-07-01 03:37:01 +0700

    ci: add mocha tests

---
+++ test/mocha.test.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+const Disgrow = require("disgrow");
+const { bot_id } = require("../src/config");
+const chai = require("chai");
+const bot = new Disgrow(bot_id);
+const expect = chai.expect;
+
+describe("Check the prerequisites", () => {
+  describe("Get the bots increments", () => {
+    before(async () => {
+      data = await bot.myStats();
+    });
+  
+    it("data should appear in the topgg", () => {
+      expect(data.data).to.be.a("object");
+    });
+  
+    it("data should have a key called 'title'", () => {
+      expect(data.data).to.have.property("title");
+    });
+  
+    it("data should have a key called 'total_votes'", () => {
+      expect(data.data).to.have.property("total_votes");
+    });
+  
+    it("data should have a key called 'server_count'", () => {
+      expect(data.data).to.have.property("server_count");
+    });
+  
+    it("data should have property 'average_server_growth_daily' with value of type number", () => {
+      expect(data).to.have.property("average_server_growth_daily");
+      expect(data.average_server_growth_daily).to.be.a("number");
+    });
+  
+    it("data should have property 'average_server_growth_monthly' with value of type number", () => {
+      expect(data).to.have.property("average_server_growth_monthly");
+      expect(data.average_server_growth_monthly).to.be.a("number");
+    });
+  
+    it("data should have property 'approximate_credits_rewards_monthly' with value of type string", () => {
+      expect(data).to.have.property("approximate_credits_rewards_monthly");
+      expect(data.approximate_credits_rewards_monthly).to.be.a("string");
+    });
+  
+    it("data should have property 'approximate_server_growth_annually' with value of type number", () => {
+      expect(data).to.have.property("approximate_server_growth_annually");
+      expect(data.approximate_server_growth_annually).to.be.a("number");
+    });
+  });
+});
