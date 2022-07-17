273e4e40b63e6c5a2b6191538a96890157051452
sinkaroid <bibsindra@gmail.com>
2022-07-18 05:56:45 +0700

    docs: make config comments looks clearly

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   botId: "724047481561809007", // Your bot's ID
-  memCacheExpire: 30, // how long to keep the in-memory cache data (in minutes)
-  alertOnFetch: false // alert when the API is called when memCache is expired
+  memCacheExpire: 30, // How long to keep the data in memory cache (in minutes)
+  alertOnFetch: false // Alert when API is called, then memCache was expired
 };
