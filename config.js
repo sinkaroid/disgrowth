dcc333d3a1c3caa6cf15ff9c2a449068a8f2ff03
sinkaroid <bibsindra@gmail.com>
2022-07-18 09:33:44 +0700

    perf: proper memCacheExpire timeout

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   botId: "724047481561809007", // Your bot's ID
-  memCacheExpire: 30, // How long to keep the data in memory cache (in minutes)
+  memCacheExpire: 60, // How long to keep the data in memory cache (in minutes)
   alertOnFetch: false // Alert when API is called, then memCache was expired
 };
