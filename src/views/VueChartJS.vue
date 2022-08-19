01c2165836a4f87948d113a8be1565ced0510302
sinkaroid <bibsindra@gmail.com>
2022-08-19 17:35:46 +0700

    fix: proper using fonts

--- src/views/VueChartJS.vue
+++ src/views/VueChartJS.vue
@@ -465,7 +465,7 @@ export default {
   border-collapse: collapse;
 
   font-size: 0.9em;
-  font-family: sans-serif;
+  font-family: "Helvetica Neue", Helvetica;
   min-width: 400px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
 }
